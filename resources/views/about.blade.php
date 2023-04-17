8e11e9474d067dd9fd1525e91ad87024be5f7a17
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-17 14:49:03 +0700

    3. struktur forder, routes & view

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<style></style>
+	<title>Hafiz Blog | About </title>
+</head>
+<body>
+	<h1>About Page</h1>
+	<h3><?php echo $name; ?></h3>
+	<p><?php echo $email; ?></p>
+	<img src="img/<?php echo $image; ?>" alt="hafiz" width="120" height="160">
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+			// Associative array
+			"name" => "Muhammad Hafiz",
+			"email" => "mhafiz.aa@gmail.com",
+			"image" => "foto.jpg"
+		]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
