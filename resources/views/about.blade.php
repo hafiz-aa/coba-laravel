dc077b657ec2846947a335f3e245d10807b2b45b
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-17 16:47:17 +0700

    4. Blade Templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<style></style>
-	<title>Hafiz Blog | About </title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('container')
 	<h1>About Page</h1>
-	<h3><?php echo $name; ?></h3>
-	<p><?php echo $email; ?></p>
-	<img src="img/<?php echo $image; ?>" alt="hafiz" width="120" height="160">
-</body>
-</html>
+	<h3>{{ $name }}</h3>
+	<p> {{  $email }}</p>
+	<img src="img/{{ $image }}" alt="{{  $name  }}" width="120" height="160">
+@endsection
