a774bcbd6c52741fa4871db95b34cf5bb5f2df28
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-23 19:07:22 +0700

    Create Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,52 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::create([
+					'name' => 'Muhammad Hafiz',
+					'email' => 'mhafiz.aa@gmail.com',
+					'password' => bcrypt('12345')
+				]);
+        User::create([
+					'name' => 'Annisa Rahmat',
+					'email' => 'annisa@gmail.com',
+					'password' => bcrypt('12345')
+				]);
+
+				Category::create([
+					'name' => 'Web Programming',
+					'slug' => 'web-programming'
+				]);
+				Category::create([
+					'name' => 'Data Science',
+					'slug' => 'data-science'
+				]);
+				Category::create([
+					'name' => 'Self Development',
+					'slug' => 'self-development'
+				]);
+
+				Post::create([
+					'title' => 'Judul Pertama',
+					'slug' => 'judul-pertama',
+					'category_id' => 1,
+					'user_id' => 1,
+					'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis,',
+					'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis, labore eligendi dicta laboriosam sint nemo unde earum provident eaque iusto atque accusantium adipisci ut fuga eius. Veritatis, tempora facere. Autem, iure! Tenetur voluptatem illum similique repellendus mollitia, itaque qui.</p><p>Soluta similique, iusto eveniet inventore dolore non porro quos ex adipisci omnis, quae ducimus voluptates. Est voluptates soluta, corporis vero obcaecati explicabo earum cum exercitationem quo dicta dolorem et molestias eligendi rem optio placeat asperiores dolor delectus reprehenderit velit facere repellat nihil. Magnam fuga, deleniti corrupti, ullam iure debitis ipsam quos voluptates, veniam explicabo impedit!</p><p>Tempore aperiam amet quis placeat, fuga nesciunt maxime recusandae neque magni, vitae, earum est ad sunt porro saepe. Harum ullam dolorum beatae accusantium quisquam, eaque ab error architecto nostrum perspiciatis. Dolorum quas sequi enim! Necessitatibus ex harum magnam nam omnis aperiam.</p>']);
+				Post::create([
+					'title' => 'Judul Kedua',
+					'slug' => 'judul-kedua',
+					'category_id' => 3,
+					'user_id' => 2,
+					'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis,',
+					'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis, labore eligendi dicta laboriosam sint nemo unde earum provident eaque iusto atque accusantium adipisci ut fuga eius. Veritatis, tempora facere. Autem, iure! Tenetur voluptatem illum similique repellendus mollitia, itaque qui.</p><p>Soluta similique, iusto eveniet inventore dolore non porro quos ex adipisci omnis, quae ducimus voluptates. Est voluptates soluta, corporis vero obcaecati explicabo earum cum exercitationem quo dicta dolorem et molestias eligendi rem optio placeat asperiores dolor delectus reprehenderit velit facere repellat nihil. Magnam fuga, deleniti corrupti, ullam iure debitis ipsam quos voluptates, veniam explicabo impedit!</p><p>Tempore aperiam amet quis placeat, fuga nesciunt maxime recusandae neque magni, vitae, earum est ad sunt porro saepe. Harum ullam dolorum beatae accusantium quisquam, eaque ab error architecto nostrum perspiciatis. Dolorum quas sequi enim! Necessitatibus ex harum magnam nam omnis aperiam.</p>']);
+				Post::create([
+					'title' => 'Judul Ketiga',
+					'slug' => 'judul-ketiga',
+					'category_id' => 1,
+					'user_id' => 1,
+					'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis,',
+					'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis, labore eligendi dicta laboriosam sint nemo unde earum provident eaque iusto atque accusantium adipisci ut fuga eius. Veritatis, tempora facere. Autem, iure! Tenetur voluptatem illum similique repellendus mollitia, itaque qui.</p><p>Soluta similique, iusto eveniet inventore dolore non porro quos ex adipisci omnis, quae ducimus voluptates. Est voluptates soluta, corporis vero obcaecati explicabo earum cum exercitationem quo dicta dolorem et molestias eligendi rem optio placeat asperiores dolor delectus reprehenderit velit facere repellat nihil. Magnam fuga, deleniti corrupti, ullam iure debitis ipsam quos voluptates, veniam explicabo impedit!</p><p>Tempore aperiam amet quis placeat, fuga nesciunt maxime recusandae neque magni, vitae, earum est ad sunt porro saepe. Harum ullam dolorum beatae accusantium quisquam, eaque ab error architecto nostrum perspiciatis. Dolorum quas sequi enim! Necessitatibus ex harum magnam nam omnis aperiam.</p>']);
+
+					
     }
 }
