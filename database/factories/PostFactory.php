67cc070391877095a1d12ad16c566273c32dc4c2
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-23 20:11:52 +0700

    Create user and post factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{	
+		protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(mt_rand(3, 6)),
+						'slug' => $this->faker->slug(),
+						'excerpt' => $this->faker->paragraph(),
+						'body' =>  $this->faker->paragraph(mt_rand(10, 25)),
+						'user_id' => mt_rand(1,5),
+						'category_id' => mt_rand(1,3),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,18 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // User::create([
-				// 	'name' => 'Muhammad Hafiz',
-				// 	'email' => 'mhafiz.aa@gmail.com',
-				// 	'password' => bcrypt('12345')
-				// ]);
-        // User::create([
-				// 	'name' => 'Annisa Rahmat',
-				// 	'email' => 'annisa@gmail.com',
-				// 	'password' => bcrypt('12345')
-				// ]);
+        User::factory(5)->create();
 
 				Category::create([
 					'name' => 'Web Programming',
@@ -41,28 +30,7 @@ class DatabaseSeeder extends Seeder
 					'slug' => 'self-development'
 				]);
 
-				Post::create([
-					'title' => 'Judul Pertama',
-					'slug' => 'judul-pertama',
-					'category_id' => 1,
-					'user_id' => 1,
-					'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis,',
-					'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis, labore eligendi dicta laboriosam sint nemo unde earum provident eaque iusto atque accusantium adipisci ut fuga eius. Veritatis, tempora facere. Autem, iure! Tenetur voluptatem illum similique repellendus mollitia, itaque qui.</p><p>Soluta similique, iusto eveniet inventore dolore non porro quos ex adipisci omnis, quae ducimus voluptates. Est voluptates soluta, corporis vero obcaecati explicabo earum cum exercitationem quo dicta dolorem et molestias eligendi rem optio placeat asperiores dolor delectus reprehenderit velit facere repellat nihil. Magnam fuga, deleniti corrupti, ullam iure debitis ipsam quos voluptates, veniam explicabo impedit!</p><p>Tempore aperiam amet quis placeat, fuga nesciunt maxime recusandae neque magni, vitae, earum est ad sunt porro saepe. Harum ullam dolorum beatae accusantium quisquam, eaque ab error architecto nostrum perspiciatis. Dolorum quas sequi enim! Necessitatibus ex harum magnam nam omnis aperiam.</p>']);
-				Post::create([
-					'title' => 'Judul Kedua',
-					'slug' => 'judul-kedua',
-					'category_id' => 3,
-					'user_id' => 2,
-					'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis,',
-					'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis, labore eligendi dicta laboriosam sint nemo unde earum provident eaque iusto atque accusantium adipisci ut fuga eius. Veritatis, tempora facere. Autem, iure! Tenetur voluptatem illum similique repellendus mollitia, itaque qui.</p><p>Soluta similique, iusto eveniet inventore dolore non porro quos ex adipisci omnis, quae ducimus voluptates. Est voluptates soluta, corporis vero obcaecati explicabo earum cum exercitationem quo dicta dolorem et molestias eligendi rem optio placeat asperiores dolor delectus reprehenderit velit facere repellat nihil. Magnam fuga, deleniti corrupti, ullam iure debitis ipsam quos voluptates, veniam explicabo impedit!</p><p>Tempore aperiam amet quis placeat, fuga nesciunt maxime recusandae neque magni, vitae, earum est ad sunt porro saepe. Harum ullam dolorum beatae accusantium quisquam, eaque ab error architecto nostrum perspiciatis. Dolorum quas sequi enim! Necessitatibus ex harum magnam nam omnis aperiam.</p>']);
-				Post::create([
-					'title' => 'Judul Ketiga',
-					'slug' => 'judul-ketiga',
-					'category_id' => 1,
-					'user_id' => 1,
-					'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis,',
-					'body' => '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, maxime. Mollitia ipsum odio debitis, labore eligendi dicta laboriosam sint nemo unde earum provident eaque iusto atque accusantium adipisci ut fuga eius. Veritatis, tempora facere. Autem, iure! Tenetur voluptatem illum similique repellendus mollitia, itaque qui.</p><p>Soluta similique, iusto eveniet inventore dolore non porro quos ex adipisci omnis, quae ducimus voluptates. Est voluptates soluta, corporis vero obcaecati explicabo earum cum exercitationem quo dicta dolorem et molestias eligendi rem optio placeat asperiores dolor delectus reprehenderit velit facere repellat nihil. Magnam fuga, deleniti corrupti, ullam iure debitis ipsam quos voluptates, veniam explicabo impedit!</p><p>Tempore aperiam amet quis placeat, fuga nesciunt maxime recusandae neque magni, vitae, earum est ad sunt porro saepe. Harum ullam dolorum beatae accusantium quisquam, eaque ab error architecto nostrum perspiciatis. Dolorum quas sequi enim! Necessitatibus ex harum magnam nam omnis aperiam.</p>']);
-
+				Post::factory(20)->create();
 					
     }
 }
