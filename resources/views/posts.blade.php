f4f1179c1e0958855199c6b5f2f8f43dcb2f6e27
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-27 10:30:24 +0700

    Update card for posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -29,9 +29,10 @@
 			@foreach ($posts->skip(1) as $post)
 				<div class="col-md-4 mb-3">
 					<div class="card">
+						<div class="position-absolute bg-dark px-3 py-2 "><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
 						<img src="https://source.unsplash.com/720x400?{{ $post->category->slug }}" class="card-img-top" alt="{{ $post->category->slug }}">
 						<div class="card-body">
-							<h5 class="card-title">{{ $post->title }}</h5>
+							<h5 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none">{{ $post->title }}</a></h5>
 								<p>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
 								</p>
 							<p class="card-text">{{ $post->excerpt }}</p>
