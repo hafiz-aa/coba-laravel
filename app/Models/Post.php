d6f37635a73b414b811a22383dbdda25169214be
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-27 14:31:21 +0700

    Add search form

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,13 +13,19 @@ class Post extends Model
 		protected $guarded = ['id'];
 		protected $with = ['category', 'author'];
 
-		public function scopeFilter($query)
+		public function scopeFilter($query, array $filters)
 		{
-			if(request('search')){
-				return $query->where('title', 'like', '%' . request('search') . '%')
-				             ->orWhere('body', 'like', '%' . request('search') . '%');
-			}
+			$query->when($filters['search'] ?? false, function($query, $search){
+				return $query->where('title', 'like', '%' . $search . '%')
+				             ->orWhere('body', 'like', '%' . $search . '%');
+			});
 
+			$query->when($filters['category'] ?? false, function($query, $category){
+				return $query->whereHas('category', function($query) use($category)
+				{
+					$query->where('slug', $category);
+				});
+			});
 		}
 
 		public function category()
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,6 +8,9 @@
 	<div class="row mb-3 justify-content-center">
 		<div class="col-md-6">
 			<form action="/posts" >
+				@if (request('category'))
+					<input type="hidden" name="category" value="{{ request('category') }}">
+				@endif
 				<div class="input-group mb-3">
 					<input type="text" class="form-control" placeholder="Search" name="search" value="{{ request('search') }}">
 					<button class="btn btn-primary" type="submit" >Search</button>
@@ -22,7 +25,7 @@
 			<div class="card-body text-center">
 				<h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0] -> title }}</a></h3>
 				<small class="text-muted">
-					<p>By: <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+					<p>By: <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}" class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
 					</p>
 				</small>
 
@@ -38,7 +41,7 @@
 			@foreach ($posts->skip(1) as $post)
 				<div class="col-md-4 mb-3">
 					<div class="card">
-						<div class="position-absolute bg-dark px-3 py-2 "><a href="/categories/{{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
+						<div class="position-absolute bg-dark px-3 py-2 "><a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
 						<img src="https://source.unsplash.com/720x400?{{ $post->category->slug }}" class="card-img-top" alt="{{ $post->category->slug }}">
 						<div class="card-body">
 							<h5 class="card-title"><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h5>
