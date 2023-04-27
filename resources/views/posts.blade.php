938644dce4cf704ae8ad2a4393803f168fa12aeb
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-27 13:08:41 +0700

    Add search form and query

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,18 @@
 @extends('layouts.main')
 
 @section('container')
-	<h1 class="mb-5">{{ $title }} </h1>
+	<h1 class="mb-3 text-center">{{ $title }} </h1>
+
+	<div class="row mb-3 justify-content-center">
+		<div class="col-md-6">
+			<form action="/posts" >
+				<div class="input-group mb-3">
+					<input type="text" class="form-control" placeholder="Search" name="search" value="{{ request('search') }}">
+					<button class="btn btn-primary" type="submit" >Search</button>
+				</div>
+			</form>
+		</div>
+	</div>
 
 	@if ($posts -> count())
 		<div class="card mb-3">
@@ -20,9 +31,7 @@
 				<a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary">Read more...</a>
 			</div>
 		</div>
-	@else
-		<p class="text-center fs-4">No post found.</p>
-	@endif
+
 
 	<div class="container">
 		<div class="row">
@@ -44,4 +53,8 @@
 		</div>
 	</div>
 	
+	@else
+	<p class="text-center fs-4">No post found.</p>
+	@endif
+
 @endsection
