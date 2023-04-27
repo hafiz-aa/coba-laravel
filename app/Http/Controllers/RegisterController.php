228dcdba13be4f4dc8188c899914d6e1bf5f2875
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-27 18:10:47 +0700

    Validation input

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,6 +15,13 @@ class RegisterController extends Controller
 		}
     public function store(Request $request)
 		{
-			return $request->all();
+			return $request->validate([
+				'name' => 'required|min:5|max:255|alpha',
+				'username' => ['required', 'min:5', 'max:255', 'unique:users' ],
+				'email' => 'required|email:dns|unique:users',
+				'password' => 'required|min:6|max:255'
+
+			]);
+
 		}
 }
