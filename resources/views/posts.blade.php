8279d88d34bdf8f26bd4392212c827e9edb42cdb
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-27 10:11:40 +0700

    Create card for each post

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
 
 	@if ($posts -> count())
 		<div class="card mb-3">
-			<img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->slug }}" class="card-img-top" alt="...">
+			<img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->slug }}" class="card-img-top" alt="{{ $posts[0]->category->slug }}">
 			<div class="card-body text-center">
 				<h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0] -> title }}</a></h3>
 				<small class="text-muted">
@@ -24,19 +24,23 @@
 		<p class="text-center fs-4">No post found.</p>
 	@endif
 
-
-
-	@foreach ($posts->skip(1) as $post)
-		<article class="mb-5 border-bottom pb-3">
-			<h2>
-				<a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
-			</h2>
-
-			<p>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-			
-			<p>{{ $post->excerpt }}</p>
-
-			<a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
-		</article>
-	@endforeach
+	<div class="container">
+		<div class="row">
+			@foreach ($posts->skip(1) as $post)
+				<div class="col-md-4 mb-3">
+					<div class="card">
+						<img src="https://source.unsplash.com/720x400?{{ $post->category->slug }}" class="card-img-top" alt="{{ $post->category->slug }}">
+						<div class="card-body">
+							<h5 class="card-title">{{ $post->title }}</h5>
+								<p>By: <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
+								</p>
+							<p class="card-text">{{ $post->excerpt }}</p>
+							<a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
+						</div>
+					</div>
+				</div>
+			@endforeach
+		</div>
+	</div>
+	
 @endsection
