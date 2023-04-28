e641f34c2d463a351e2f5bfebc2c302cdb82a123
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-28 10:14:41 +0700

    Create route & method for login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -14,16 +14,23 @@
 		
 		<main class="form-signin w-100 m-auto">
 			<h1 class="h3 mb-3 fw-normal text-center">Please Log In</h1>
-			<form>
+			<form action="/login"  method="post">
+				@csrf
 				{{-- <img class="mb-4" src="../assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> --}}
-		
 				<div class="form-floating">
-					<input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-					<label for="floatingInput">Email address</label>
+					<input type="email" name="email" class="form-control @error('email')
+						is-invalid
+					@enderror" id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+					<label for="email">Email address</label>
+					@error('email')
+						<div class="invalid-feedback">
+							{{ $message }}
+						</div>
+					@enderror
 				</div>
 				<div class="form-floating">
-					<input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-					<label for="floatingPassword">Password</label>
+					<input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+					<label for="password">Password</label>
 				</div>
 		
 				<button class="w-100 btn btn-lg btn-primary" type="submit">Log in</button>
