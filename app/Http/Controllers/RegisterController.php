7b3c342dd317ce302cbd13f9bd25a34d66b3ec81
hafiz-aa <mhafiz.aa@gmail.com>
2023-04-28 09:06:24 +0700

    Insert New User

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -15,13 +17,17 @@ class RegisterController extends Controller
 		}
     public function store(Request $request)
 		{
-			return $request->validate([
-				'name' => 'required|min:5|max:255|alpha',
+			$validatedData = $request->validate([
+				'name' => 'required|min:5|max:255',
 				'username' => ['required', 'min:5', 'max:255', 'unique:users' ],
 				'email' => 'required|email:dns|unique:users',
 				'password' => 'required|min:6|max:255'
-
 			]);
+
+			// $validatedData['password'] = bcrypt($validatedData['password']);
+			$validatedData['password'] = Hash::make($validatedData['password']);
+
+			User::create($validatedData);
 
 		}
 }
