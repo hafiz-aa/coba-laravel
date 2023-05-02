18a7d0f9df5cd804633d20a3a9fe7209f8658064
hafiz-aa <mhafiz.aa@gmail.com>
2023-05-02 13:08:43 +0700

    Create validation for form create

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -34,7 +34,12 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        $validateData = $request->validate([
+					'title' => 'required|max:255',
+					'slug' => 'required|unique:posts',
+					'category_id' => 'required',
+					'body' => 'required',
+				]);
     }
 
     /**
