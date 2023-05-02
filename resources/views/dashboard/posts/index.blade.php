53182023f41f8d6eccda9b644eafe7ff5c949db1
hafiz-aa <mhafiz.aa@gmail.com>
2023-05-02 14:56:22 +0700

    Create delete feature

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -31,7 +31,13 @@
 				<td>
 					<a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye" class="align-text-bottom"></span></a>
 					<a href="" class="badge bg-warning"><span data-feather="edit" class="align-text-bottom"></span></a>
-					<a href="" class="badge bg-danger"><span data-feather="x-circle" class="align-text-bottom"></span></a>
+					<form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+						@method('delete')
+						@csrf
+						<button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')" >
+							<span data-feather="x-circle" class="align-text-bottom"></span>
+						</button>
+					</form>
 				</td>
 			</tr>
 			@endforeach
